b66947930a88cdac858c06f74253af3dfbad827c
Lance Kindle <lance.kindle@gmail.com>
2017-11-02 11:57:05 -0700

    add DMA routine to sprite example
    sprite properties are now updates each vblank by DMA. We set aside 160
    bytes starting at $C000 (_RAM) to use as sprites. This chunk of ram is
    then copied to the actual OAM ram $FE00 each vblank. This is nice since
    it stops our main game logic from having to wait each vblank to update
    sprite positions. We still, currently, because that limits the movement
    speed of the sprite

--- 03_good_sprite_moves.asm
+++ 03_good_sprite_moves.asm
@@ -1,5 +1,9 @@
 include "gbhw.inc"
 
+DMA_ROUTINE	= $FF80
+OAMDATALOC	= _RAM	; set first 160 bytes of RAM to hold OAM variables
+OAMDATALOCBANK	= OAMDATALOC / $100 ; used by DMA_ROUTINE to point to _RAM
+
 ;-------------- INTERRUPT VECTORS ------------------------
 ; specific memory addresses are called when a hardware interrupt triggers
 
@@ -7,7 +11,7 @@ include "gbhw.inc"
 ; (VRAM) is only available during VBLANK. So this is when updating OAM /
 ; sprites is executed.
 SECTION "Vblank", ROM0[$0040]
-	reti
+	JP	DMA_ROUTINE
 
 SECTION "LCDC", ROM0[$0048]
 	reti
@@ -30,36 +34,68 @@ SECTION "ROM_entry_point", ROM0[$0100]	; ROM is given control from boot here
 ; The gameboy reads this info (before handing control over to ROM)
 SECTION "rom header", ROM0[$0104]
 	NINTENDO_LOGO
-	ROM_HEADER	"0123456789ABCDE"
+	ROM_HEADER	"first dma usage"
 
-; safe to include other files begining here. INCLUDE'd files often immediately
-; add more code to the compiled ROM. It's critical that your code does not
-; step over the first $0000 - $014E bytes
+
+dma_Copy2HRAM: MACRO
+; copies the dmacode to HIRAM. dmacode will get run each Vblank,
+; and it is resposible for copying sprite data from ram to vram.
+; dma_Copy2HRAM trashes all registers
+; actual dma code preserves all registers
+	jr	.copy_dma_into_memory\@
+.dmacode\@
+	push	af
+	ld	a, OAMDATALOCBANK
+	ldh	[rDMA], a
+	ld	a, $28 ; countdown until DMA is finishes, then exit
+.dma_wait\@			;<-|
+	dec	a		;  |	keep looping until DMA finishes
+	jr	nz, .dma_wait\@ ; _|
+	pop	af
+	reti	; if this were jumped to by the v-blank interrupt, we'd
+		; want to reti (re-enable interrupts).
+.dmaend\@
+.copy_dma_into_memory\@
+	ld	de, DMA_ROUTINE
+	ld	hl, .dmacode\@
+	ld	bc, .dmaend\@ - .dmacode\@
+; mem_Copy copied from memory.asm for this example
+; copies BC # of bytes from source (HL) to destination (DE)
+.mem_Copy\@
+	inc	b
+	inc	c
+	jr	.skip\@
+.loop\@	ld	a,[hl+]
+	ld	[de],a
+	inc	de
+.skip\@	dec	c
+	jr	nz,.loop\@
+	dec	b
+	jr	nz,.loop\@
+	ENDM
 
 
 code_begins:
 	di	; disable interrupts
 	ld	SP, $FFFF	; set stack to top of HRAM
-; background image is just the nintendo logo. Tile 0x19 is the (R).
-; You can find it on BGB's VRAM Tiles: (right-click) -> other -> VRAM -> Tiles
-; Let's use the (R) as a sprite and move it
 
+	dma_Copy2HRAM	; routine that updates sprites for us
 
 	ld	a, IEF_VBLANK	; --
 	ld	[rIE], a	; Set only Vblank interrupt flag
 	ei			; enable interrupts. Only vblank will trigger
 
+	ld	a, [rLCDC]	; fetch LCD Config. (Each bit is a flag)
+	or	LCDCF_OBJON	; enable sprites through "OBJects ON" flag
+	or	LCDCF_OBJ8	; enable 8bit wide sprites (vs. 16-bit wide)
+	ld	[rLCDC], a	; save LCD Config. Sprites are now visible. 
 
-	halt	; wait a frame
-	nop
-	halt
-	nop	; wait two frames before pushing bytes -- critical timing here
-	; if we mess up timing, writes to OAM will fail. Thus the sprite will
-	; NOT be updated. Try removing these two halts to see what happens.
-
+	; don't need to wait for vblank since DMA_routine is now called
+	; automatically when that happens
 
 ; ----
-; OAM aka Sprite memory on the gameboy begins at memory address $FE00.
+; DMA_ROUTINE called each vblank now handles moving data data starting at
+; _RAM ($C000) into $FE00. So now we write sprite data at $C000
 ; There are a total of 40 sprites available to manipulate.
 ; Each sprite has 4 attributes that are set in sequential bytes in memory:
 ;	X coordinate
@@ -67,19 +103,15 @@ code_begins:
 ;	Tile #  (relative to start of tiles in VRAM: $8000)
 ;	Sprite Flags  (such as, visible, priority, X & Y flip)
 
-	ld	hl, _OAMRAM	; point to 1st sprite's 1st property: X
+	ld	hl, _RAM	; point to 1st sprite's 1st property: X
 	ld	[hl], 20	; set X to 20
-	ld	hl, _OAMRAM + 1 ; HL points to sprite's Y
+	inc	hl		; HL points to sprite's Y
 	ld	[hl], 10	; set Y to 10
-	ld	hl, _OAMRAM + 2 ; HL points to sprite's tile (from BG map)
+	inc	hl		; HL points to sprite's tile (from BG map)
 	ld	[hl], $19	; set Tile to the (R) graphic
-	ld	hl, _OAMRAM + 3	; HL points to sprite's flags
+	inc	hl		; HL points to sprite's flags
 	ld	[hl], 0		; set all flags to 0. X,Y-flip, palette, etc.
 
-	ld	a, [rLCDC]	; fetch LCD Config. (Each bit is a flag)
-	or	LCDCF_OBJON	; enable sprites through "OBJects ON" flag
-	or	LCDCF_OBJ8	; enable 8bit wide sprites (vs. 16-bit wide)
-	ld	[rLCDC], a	; save LCD Config. Sprites are now visible. 
 
 .loop
 	halt	; halts cpu until interrupt triggers (vblank)
@@ -87,17 +119,31 @@ code_begins:
 	; so only 60fps. That makes the sprite movement here manageable
 	nop
 
-	ld	hl, _OAMRAM
+	ld	hl, _RAM	; HL points to X coordinate
 	ld	a, [hl]
 	inc	a		; X += 1
 	ld	[hl], a		; save new X coordinates
-	ld	hl, _OAMRAM + 1
+	inc	hl		; HL points to Y coordinate
 	inc	[hl]		; set Y += 1
 
 	jp	.loop		; start up at top of .loop label. Repeats each vblank
 
-; ================ QUESTIONS FOR STUDENT ===========================
-; why is there visual junk on the screen?
-; can you change this so that Y moves twice as fast as X?
-; can you change this so that sprite moves half as fast?
-; what happens if you remove the "halt" command in the .loop? Why?
+; =============================================================
+; ----------- (In Depth) So what does DMA do? -----------------
+; =============================================================
+; DMA (on the original gameboy) is a specific memory-copying routine
+; that is 2x+ faster at copying memory from a specified source to a
+; hard-coded destination of $FE00-$FE9F (where OAM resides).
+; because sprites / objects depend on OAM manipulation, we write to
+; OAM all throughout game logic. Except there's a catch: OAM is
+; inaccessible a lot due to the LCD drawing routine. So almost every
+; game sets aside 160 bytes in normal RAM that they write to. Then,
+; every Vertical-Blank (when OAM is accessible), we initiate DMA to
+; copy the 160 bytes from RAM into OAM. The value we write to [rDMA]
+; is the MSB version of the source address. If we write $C0,
+; then DMA copies 160 bytes from $C000 into OAM.
+; while DMA performs it's copying routine, the cpu is still active,
+; but unable to access the ROM, or any other memory aside from HRAM.
+; That's why this routine gets copied into HRAM and performs a
+; for-loop before exiting. It's timed so that DMA is complete when
+; it exits. In this case, DMA takes 162 Machine Cycles.
