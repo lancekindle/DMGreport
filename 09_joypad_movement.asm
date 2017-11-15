4fd1b1ab83f10eb857e829d89b8cdefa5bd70526
Lance Kindle <lance.kindle@gmail.com>
2017-11-15 11:04:06 -0800

    add joypad reading and movement

--- 09_joypad_movement.asm
+++ 09_joypad_movement.asm
@@ -55,22 +55,8 @@ code_begins:
 	or	LCDCF_OBJ8	; enable 8bit wide sprites (vs. 16-bit wide)
 	ld	[rLCDC], a	; save LCD Config. Sprites are now visible. 
 
-	; don't need to wait for vblank since DMA_routine is now called
-	; automatically when that happens
-
-; ----
-; DMA_ROUTINE is called each vblank now handles moving data data starting at
-; _RAM ($C000) into $FE00. So now we write sprite data at $C000
-; There are a total of 40 sprites available to manipulate.
-; Each sprite has 4 attributes that are set in sequential bytes in memory:
-;	X coordinate
-;	Y coordinate
-;	Tile #  (relative to start of tiles in VRAM: $8000)
-;	Sprite Flags  (such as, visible, priority, X & Y flip)
-
 
 	; see where we declare "copyright" as a sprite-variable above
-
 	; set X=20, Y=10, Tile=$19, Flags=0
 	PutSpriteXAddr	copyright, 20
 	PutSpriteYAddr	copyright, 10
@@ -84,14 +70,78 @@ code_begins:
 	; so only 60fps. That makes the sprite movement here manageable
 	nop
 
-	; move copyright symbol diagonally across screen
+	call	jpad_GetKeys
+
+	; move character if corresponding button has been pushed
+	push	af	; save register A (joypad info)
+	and	PADF_UP	; compare joypad info. Set NZ flag if UP bit present
+	jr	z, .skip_up
+	GetSpriteYAddr	copyright
+	dec	A
+	PutSpriteYAddr	copyright, a
+.skip_up
+	pop	af	; restore register A (joypad info)
+	push	af	; save (again) reg. A
+	and	PADF_DOWN
+	jr	z, .skip_down
+	GetSpriteYAddr	copyright
+	inc	A
+	PutSpriteYAddr	copyright, a
+.skip_down
+	pop	af
+	push	af
+	and	PADF_LEFT
+	jr	z, .skip_left
+	GetSpriteXAddr	copyright
+	dec	A
+	PutSpriteXAddr	copyright, a
+.skip_left
+	pop	af
+	push	af
+	and	PADF_RIGHT
+	jr	z, .skip_right
 	GetSpriteXAddr	copyright
 	inc	A
-	PutSpriteXAddr	copyright, A
-
-	GetSpriteYAddr	copyright ; macro GetSpr... puts Y value in A register
-	inc	A
-	PutSpriteYAddr	copyright, A
-
+	PutSpriteXAddr	copyright, a
+.skip_right
+	pop	af
 
 	jp	.loop		; start up at top of .loop label. Repeats each vblank
+
+
+jpad_GetKeys:
+; Uses AF, B
+; get currently pressed keys. Register A will hold keys in the following
+; order: MSB --> LSB (Most Significant Bit --> Least Significant Bit)
+; Down, Up, Left, Right, Start, Select, B, A
+; This works by writing
+
+	; get action buttons: A, B, Start / Select
+	ld	a, JOYPAD_BUTTONS; choose bit that'll give us action button info
+	ld	[rJOYPAD], a; write to joypad, telling it we'd like button info
+	ld	a, [rJOYPAD]; gameboy will write (back in address) joypad info
+	ld	a, [rJOYPAD]
+	cpl		; take compliment
+	and	$0f	; look at first 4 bits only  (lower nibble)
+	swap	a	; place lower nibble into upper nibble
+	ld	b, a	; store keys in b
+	; get directional keys
+	ld	a, JOYPAD_ARROWS
+	ld	[rJOYPAD], a ; write to joypad, selecting direction keys
+	ld	a, [rJOYPAD]
+	ld	a, [rJOYPAD]
+	ld	a, [rJOYPAD]	; delay to reliablly read keys
+	ld	a, [rJOYPAD]	; since we've just swapped from reading
+	ld	a, [rJOYPAD]	; buttons to arrow keys
+	ld	a, [rJOYPAD]
+	cpl			; take compliment
+	and	$0f		; keep lower nibble
+	or	b		; combine action & direction keys (result in a)
+	ld	b, a
+
+	ld	a, JOYPAD_BUTTONS | JOYPAD_ARROWS
+	ld	[rJOYPAD], a		; reset joypad
+
+	ld	a, b	; register A holds result. Each bit represents a key
+	ret
+
